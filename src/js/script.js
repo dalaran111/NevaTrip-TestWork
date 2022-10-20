5ff8cdadc15f372c0d714a7b72423721688d33ff
Dalaran111 <dalaran111@gmail.com>
2022-10-20 20:15:27 +0300

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+const pricesMain = document.querySelectorAll('.cards__prices-main'),
+      pricesPier = document.querySelectorAll('.cards__prices-pier'),
+      yellowSticker = document.querySelector('.yellow__sticker'),
+      noneSticker = document.querySelector('.none__sticker');
+
+function priceChanger() {
+    if (window.innerWidth > 768) {
+        pricesMain.forEach(e => {
+            e.textContent = "900 \u20bd";
+        });
+        pricesPier.forEach(e => {
+            e.textContent = "1200 \u20bd на причале";
+        });
+    }
+}
+function stickerChanger() {
+    if (window.innerWidth > 992) {
+        yellowSticker.childNodes[0].textContent = "КРУГЛЫЙ ГОД";
+        noneSticker.style.display = "none";
+    }
+}
+
+
+window.addEventListener("resize", priceChanger, "once");
+window.addEventListener("resize", stickerChanger, "once");
+window.addEventListener("load", priceChanger);
+window.addEventListener("load", stickerChanger);
+
+
